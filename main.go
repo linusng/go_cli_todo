a88b2c2869d3970c6acbea3d4c41dc4225089057
Linus Ng <linusng@outlook.com>
2025-01-02 16:03:05 +0800

    added remove, clear, and done functions

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
   "encoding/json"
   "fmt"
   "os"
-  // "strconv"
+  "strconv"
 )
 
 type Task struct {
@@ -52,6 +52,33 @@ func main() {
       }
       fmt.Printf("%d. %s %s\n", i+1, status, task.Description)
     }
+  case "done":
+    if len(os.Args) < 3 {
+      fmt.Println("Usage: todo one [task number]")
+      return
+    }
+    index, _ := strconv.Atoi(os.Args[2])
+    if index > 0 && index <= len(tasks) {
+      tasks[index-1].Completed = true
+      fmt.Println("Task marked as complete.")
+    } else {
+      fmt.Println("Invalid task number.")
+    }
+  case "remove":
+    if len(os.Args) < 3 {
+      fmt.Println("Usage: todo remove [task number]")
+      return
+    }
+    index, _ := strconv.Atoi(os.Args[2])
+    if index > 0 && index <= len(tasks) {
+      tasks = append(tasks[:index-1], tasks[index:]...)
+      fmt.Println("Task removed.")
+    } else {
+      fmt.Println("Invalid task number.")
+    }
+  case "clear":
+    tasks = nil
+    fmt.Println("All tasks cleared.")
   case "help":
     fmt.Println(`Usage:
     'todo add [task]' - Add new task
